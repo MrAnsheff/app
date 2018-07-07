dff85206440b809a32e1ccc794e89bebf49c90c1
MrAnsheff <mransheff86@gmail.com>
2018-07-07 10:27:52 +0400

    First State

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,41 @@ main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+    State<StatefulWidget> createState() {
+      // TODO: implement createState
+      return _MyAppState();
+    }
+}
+
+class _MyAppState extends State<MyApp>{
   @override
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('EasyList'),
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: Column(children: [
+          Container(
+            margin: EdgeInsets.all(20.0),
+            child: RaisedButton(
+            onPressed: (){},
+            child: Text('Rised Button'),
+            ),
           ),
-          body: Card(
+
+          Card(
             child: Column(
               children: <Widget>[
                 Image.asset('assets/food.jpg'),
                 Text('Food Paradise')
               ],
             ),
-          )),
+          )
+        ]),
+      ),
     );
   }
 }
